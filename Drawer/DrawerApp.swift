f4e3cb99f7d04de26661b9a9e489c796010e4c10
n0n1yzh <1160562880@qq.com>
2021-03-20 16:28:05 +0800

    Initial Commit

---
+++ Drawer/DrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawerApp.swift
+//  Drawer
+//
+//  Created by YZH on 2021/3/20.
+//
+
+import SwiftUI
+
+@main
+struct DrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
